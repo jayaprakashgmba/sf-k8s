bd653c3d896223f85e8a59e22698265a1f8067ad
jp <jp@gmail.com>
2025-09-30 17:03:34 +0530

    testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,6 @@
 set -e
 
 echo "Starting Salesforce deployment inside container..."
-
-# Debug: show env vars
 echo "SF_USERNAME=$SF_USERNAME"
 echo "SF_CLIENT_ID=$SF_CLIENT_ID"
 echo "SF_ALIAS=$SF_ALIAS"
@@ -18,13 +16,16 @@ sf org login jwt \
   --alias "$SF_ALIAS" \
   --instance-url "$SF_LOGIN_URL"
 
+# Change to project root
+cd /app
+
 # Deploy using package.xml
 sf project deploy start \
-  --manifest /app/manifest/package.xml \
+  --manifest ./manifest/package.xml \
   --target-org "$SF_ALIAS" \
   --test-level "$SF_TEST_LEVEL" \
-  --tests "$TEST_CLASSES" \
+  --tests $TEST_CLASSES \
   --wait 10 \
   --verbose
 
-echo "Salesforce deployment finished!"
+echo "Deployment finished!"
